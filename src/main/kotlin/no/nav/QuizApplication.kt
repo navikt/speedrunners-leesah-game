cbfe8d631922f593067a5a7ac523dbce6bb3d064
celinabs <celina-bs@hotmail.com>
2022-06-23 15:26:46 +0200

    Update QuizApplication.kt

--- src/main/kotlin/no/nav/QuizApplication.kt
+++ src/main/kotlin/no/nav/QuizApplication.kt
@@ -36,9 +36,9 @@ class QuizApplication(private val teamName: String, database: Database? = null):
     }
 
     private fun handleAritmetic(question: Question) {
-        val n1 = Integer.parseInt(question.question.subSequence(0,2).toString())
-        val n2 = Integer.parseInt(question.question.subSequence(5,6).toString())
-        val operator = question.question[3]
+        val n1 = Integer.parseInt(question.question.substring(0,2).toString())
+        val n2 = Integer.parseInt(question.question.substring(3).toString())
+        val operator = question.question[2]
         var resultado = 0
         when (operator) {
             '+' ->  resultado = n1 + n2
