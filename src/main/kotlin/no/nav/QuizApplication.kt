62e29d6abc184d03e79b93ee21f9be2ce25f0da9
celinabs <celina-bs@hotmail.com>
2022-06-23 15:12:53 +0200

    Update QuizApplication.kt

--- src/main/kotlin/no/nav/QuizApplication.kt
+++ src/main/kotlin/no/nav/QuizApplication.kt
@@ -18,6 +18,7 @@ class QuizApplication(private val teamName: String, database: Database? = null):
     override fun handle(question: Question) {
         logger.log(question)
         if (question.category == "team-registration") handleRegisterTeam(question)
+        if (question.category == "aritmetic") handleAritmetic(question)
     }
 
 
